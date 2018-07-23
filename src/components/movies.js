e5a7f8b709f6f97ecc709bcba7d5e9fab4d1134e
Jordan Mor <jordanmor@gmail.com>
2018-07-23 12:02:31 -0400

    Create movie component

---
+++ src/components/movies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>Movies</div>
+     );
+  }
+}
+ 
+export default Movies;
