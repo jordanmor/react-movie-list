8ecb5877ca9aa4b6b307eccd0a770638657e3218
Jordan Mor <jordanmor@gmail.com>
2018-08-06 19:13:59 -0400

    Fix index.css so it takes precedence over other css files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
